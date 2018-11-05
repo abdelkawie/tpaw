7c8f9970830d6c4e78aabc60746ac9d5b84e3c28
abdelkawie <44164632+abdelkawie@users.noreply.github.com>
2018-11-05 18:32:06 +0000

    tp2

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -6,17 +6,28 @@ function validation() {
     var mail =document.querySelector("#mail").value ;
     if(nom=="" || nom.length < 5 ){
         document.querySelector("#error").textContent="le nom est obligatoire et doit contenir au moins 5 caractères ";
+        document.getElementById("error").style.display= "block";
+        document.getElementById("resultat").style.display= "none";
     }else if(prenom=="" || prenom.length < 5 ){
         document.querySelector("#error").textContent="le prenom est obligatoire et doit contenir au moins 5 caractères ";
+        document.getElementById("error").style.display= "block";
+        document.getElementById("resultat").style.display= "none";
     }else if(date=="" || date.length < 5 ){
         document.querySelector("#error").textContent="la date est obligatoire et doit contenir au moins 5 caractères ";
+        document.getElementById("error").style.display= "block";
+        document.getElementById("resultat").style.display= "none";
     }else if(adresse=="" || adresse.length < 5 ){
         document.querySelector("#error").textContent="l'adresse est obligatoire et doit contenir au moins 5 caractères ";
+        document.getElementById("error").style.display= "block";
+        document.getElementById("resultat").style.display= "none";
     }else if(mail=="" || mail.length < 5 ){
         document.querySelector("#error").textContent="l'email est obligatoire et doit contenir au moins 5 caractères ";
+        document.getElementById("error").style.display= "block";
+        document.getElementById("resultat").style.display= "none";
     } 
     else{
     document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
-    document.querySelector("#error").textContent="";
+    document.getElementById("resultat").style.display= "block";
+    document.getElementById("error").style.display= "none";
     }
 }
